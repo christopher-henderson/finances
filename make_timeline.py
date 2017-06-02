a3cb10541d87f644638cbb0224e403781176e9ea
christopher-henderson <chris@chenderson.org>
2017-06-02 15:40:15 -0700

    better code

--- make_timeline.py
+++ make_timeline.py
@@ -4,31 +4,52 @@ from datetime import datetime
 
 class Month(object):
 
-    def __init__(self, date, debt, retirement, retirement_in_market, down, down_in_market):
+    def __init__(self,
+                 date,
+                 debt,
+                 no_market_retirement,
+                 low_market_retirement,
+                 avg_market_retirement,
+                 highmarket_retirement,
+                 no_market_down,
+                 low_market_down,
+                 avg_market_down,
+                 high_market_down,):
         self.date = date
         self.debt = debt
-        self.retirement = retirement
-        rm = retirement_in_market
-        self.retirement_in_market = rm + rm * (0.06/12)
-        self.down = down
-        dm = down_in_market
-        self.down_in_market = dm + dm * (0.03/12)
+        self.no_market_retirement = no_market_retirement
+        self.low_market_retirement = low_market_retirement
+        self.avg_market_retirement = avg_market_retirement
+        self.high_market_retirement = highmarket_retirement
+        self.no_market_down = no_market_down
+        self.low_market_down = low_market_down
+        self.avg_market_down = avg_market_down
+        self.high_market_down = high_market_down
+        # self.retirement = retirement
+        # self.retirement_in_market = rm + rm * (0.06/12)
+        # self.down = down
+        # dm = down_in_market
+        # self.down_in_market = dm + dm * (0.03/12)
 
     def __str__(self):
-        return "{DATE},{SD},{R},{RM},{DOWN},{DM}".format(
+        return "{DATE},{SD},{NMR},{LMR},{MMR},{HMR},{NMD},{LMR},{MMD},{HMD}".format(
             DATE=self.date.isoformat(),
             SD=self.debt,
-            R=self.retirement,
-            RM=self.retirement_in_market,
-            DOWN=self.down,
-            DM=self.down_in_market
+            NMR=self.no_market_retirement,
+            LMR=self.low_market_retirement,
+            MMR=self.avg_market_retirement,
+            HMR=self.high_market_retirement,
+            NMD=self.no_market_down,
+            LMD=self.low_market_down,
+            MMD=self.avg_market_down,
+            HMD=self.high_market_down
         )
 
 loans = 121000
 monthlydebt = 2000
 retirement = 833
 down = 2300
-months = [Month(datetime(2018, 5, 1, 1, 1), loans, 24000, 24000, 0, 0)]
+months = [Month(datetime(2018, 5, 1, 1, 1), loans, 24000, 24000, 24000, 24000, 0, 0, 0, 0)]
 
 taken = 0
 while loans > 0:
@@ -36,12 +57,21 @@ while loans > 0:
     previous = months[-1]
     month = (previous.date.month) % 12 + 1
     year = previous.date.year + previous.date.month // 12
+    no_market_retirement = previous.no_market_retirement + retirement
+    low_market_retirement = previous.low_market_retirement * (.03 / 12) + previous.low_market_retirement + retirement
+    avg_market_retirement = previous.avg_market_retirement * (.06 / 12) + previous.avg_market_retirement + retirement
+    high_market_retirement = previous.high_market_retirement * (.09 / 12) + previous.high_market_retirement + retirement
     current = Month(
         datetime(year, month, 1, 1, 1),
         previous.debt - monthlydebt,
-        previous.retirement + retirement,
-        previous.retirement_in_market + retirement,
-        0, 0)
+        no_market_retirement,
+        low_market_retirement,
+        avg_market_retirement,
+        high_market_retirement,
+        0,
+        0,
+        0,
+        0)
     months.append(current)
     loans -= monthlydebt
 
@@ -52,17 +82,30 @@ for _ in range(5*12):
     previous = months[-1]
     month = (previous.date.month) % 12 + 1
     year = previous.date.year + previous.date.month // 12
+    no_market_retirement = previous.no_market_down + retirement
+    no_market_retirement = previous.no_market_retirement + retirement
+    low_market_retirement = previous.low_market_retirement * (.03 / 12) + previous.low_market_retirement + retirement
+    avg_market_retirement = previous.avg_market_retirement * (.06 / 12) + previous.avg_market_retirement + retirement
+    high_market_retirement = previous.high_market_retirement * (.09 / 12) + previous.high_market_retirement + retirement
+    no_market_down = previous.no_market_down + down
+    low_market_down = previous.low_market_down * (.03 / 12) + previous.low_market_down + down
+    avg_market_down = previous.avg_market_down * (.06 / 12) + previous.avg_market_down + down
+    high_market_down = previous.high_market_down * (.09 / 12) + previous.high_market_down + down
     current = Month(
         datetime(year, month, 1, 1, 1),
         0,
-        previous.retirement + retirement,
-        previous.retirement_in_market + retirement,
-        previous.down + down,
-        previous.down_in_market + down)
+        no_market_retirement,
+        low_market_retirement,
+        avg_market_retirement,
+        high_market_retirement,
+        no_market_down,
+        low_market_down,
+        avg_market_down,
+        high_market_down)
     months.append(current)
     loans -= monthlydebt
 
-csv = 'date, student_loans, retirement, down_payment\n{DATA}'.format(
+csv = 'date, student_loans, no_market_retirement, low_market_retirement, avg_market_retirement, high_market_retirement, no_market_down, low_market_down, avg_market_down, high_market_down\n{DATA}'.format(
     DATA='\n'.join([str(month) for month in months])
 )
 
